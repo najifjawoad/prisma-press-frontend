0d70edf9fe6dcf9d000b8fcc04b0603a73024fdb
najif jawoad jim <najifjawoad@gmail.com>
2026-07-26 18:59:55 +0600

    first commit

---
+++ app/(public-Group)/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function HomePage() {
+  return (
+ <div>Hello Nextjs from JIM</div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+
+
+;
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={` h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        {/* Navbar */}
+        {children}
+        {/* footer */}
+        </body>
+    </html>
+  );
+}
