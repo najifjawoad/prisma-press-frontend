aa0664362cf52f000bd4087585e7bf756efbc4eb
najif jawoad jim <najifjawoad@gmail.com>
2026-07-26 19:56:07 +0600

    28-done

--- app/(public-Group)/page.tsx
+++ app/(public-Group)/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function HomePage() {
   return (
- <div>Hello Nextjs from JIM</div>
+ <div>Hello Nextjs from JIM
+ <Button  > new btn</Button>
+
+ </div>
+
+ 
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 
 ;
@@ -11,7 +15,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
         {/* Navbar */}
