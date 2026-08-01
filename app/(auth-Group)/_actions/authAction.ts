3751b54639dab825dcdf43072522fc7e3d8f901c
najif jawoad jim <najifjawoad@gmail.com>
2026-08-02 00:53:06 +0600

    30-1

--- app/(auth-Group)/_actions/authAction.ts
+++ app/(auth-Group)/_actions/authAction.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 type loginState ={
     success: true,
@@ -53,7 +54,22 @@ export const logInAction = async  (previousState  : loginState, formData: FormDa
         sameSite : "lax",
     })
 
-    redirect("/dashboard")
+    const decodedToken = jwt.decode(result.data.accessToken) as JwtPayload;
+    if(decodedToken.role === "USER")
+    {
+      redirect("/dashboard")
+
+      }
+      else if(decodedToken.role === "ADMIN")
+      {
+        redirect("admin-dashboard")
+      }
+      else if(decodedToken.role === "AUTHOR")
+      {
+        redirect("author-dashboard")
+      }
+
+  
   }
 
 
